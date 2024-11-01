0a189931d660c7702e312556eb8b0021115b8afe
Madhukunchala0607 <madkunch@gmail.com>
2024-11-01 14:23:46 +0530

    changes

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// Select hamburger and mobile menu elements
+
 const hamburger = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.mobile-menu');
 
-// Toggle the mobile menu visibility
+
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     mobileMenu.classList.toggle('active');

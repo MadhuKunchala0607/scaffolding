b94c1024b475c975682c7954104862c0c135e706
Madhukunchala0607 <madkunch@gmail.com>
2024-11-03 20:23:06 +0530

    almost there

--- script.js
+++ script.js
@@ -6,3 +6,35 @@ function toggleAnswer(questionElement) {
         answerElement.style.display = "block";
     }
 }
+document.addEventListener("DOMContentLoaded", function () {
+    const scrollElements = document.querySelectorAll('.scroll, .scrollc');
+
+    const elementInView = (el, offset = 100) => {
+        const elementTop = el.getBoundingClientRect().top;
+        return (
+            elementTop <= (window.innerHeight || document.documentElement.clientHeight) - offset
+        );
+    };
+
+    const displayScrollElement = (element) => {
+        element.classList.add('animate');
+    };
+
+    const hideScrollElement = (element) => {
+        element.classList.remove('animate');
+    };
+
+    const handleScrollAnimation = () => {
+        scrollElements.forEach((el) => {
+            if (elementInView(el, 150)) {
+                displayScrollElement(el);
+            } else {
+                hideScrollElement(el);
+            }
+        });
+    };
+
+    window.addEventListener('scroll', () => {
+        handleScrollAnimation();
+    });
+});
